933617872d982578332d077c67695a34e92c9359
jisna19 <jisnavarghese99@gmail.com>
2024-06-10 15:35:52 +0530

    completed

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+const app=express()
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("welcome")
+})
+
+app.listen(8080,()=>{
+    console.log("Server started")
+})
